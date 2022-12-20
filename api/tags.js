e76948530bb52851feb5bc79d96e47bfb94931c5
Collin Weaver <weaverscajunrenovations@gmail.com>
2022-12-20 17:50:50 -0600

    part 3 juicebox part 2 finished

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -14,6 +14,24 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+//==============LOOK HERE LOOK HERE LOOK HERE=========
+//not passing the error messages correctly, come back
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+    const posts = await getPostsByTagName(tagName);
+    if (posts) {
+      res.send(posts);
+    } else {
+      next({
+        name: "NoPostsError",
+        message: "No posts with that tag",
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -228,6 +228,14 @@ const getPostById = async (postId) => {
     `,
       [postId]
     );
+
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId",
+      };
+    }
+
     //grabs tags for post where postId and tagId are the same
     const { rows: tags } = await client.query(
       `
@@ -363,4 +371,5 @@ module.exports = {
   getPostsByTagName,
   getAllTags,
   getUserByUsername,
+  getPostById,
 };
